c4fdd81250d0be61667611152f395741216668a6
amine <boughabaamine76@gmail.com>
2025-08-31 13:57:32 +0100

    Route Params

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import express from "express";
 
 const app = express();
+const users = [
+    { id: 1, name: "Amine" },
+    { id: 2, name: "Zeki" },
+    { id: 3, name: "Abdnour" },
+];
+const products = [
+    { id: 1, name: "Product1", price: 120.0 },
+    { id: 2, name: "Product2", price: 130.0 },
+    { id: 3, name: "Product3", price: 140.0 },
+];
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -8,21 +18,23 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-    res.status(201).send({ msg: "Hello, Wolrd" });
+    res.status(201).send({msg: "Welcom to my app"});
 });
 
 app.get("/api/users", (req, res) => {
-    res.send([
-        { id: 1, name: "Amine" },
-        { id: 2, name: "Zeki" },
-        { id: 3, name: "Abdnour" },
-    ]);
+    res.send(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    res.send(users.find((user) => user.id === id));
 });
 
 app.get("/api/products", (req, res) => {
-    res.send([
-        { id: 1, name: "Product1", price: 120.0 },
-        { id: 2, name: "Product2", price: 130.0 },
-        { id: 3, name: "Product3", price: 140.0 },
-    ]);
-})
+    res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    res.send(products.find((product) => product.id === id));
+});
