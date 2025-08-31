a54bdb228b756c09bc8caa46f9afa012e51ad438
amine <boughabaamine76@gmail.com>
2025-08-31 13:36:10 +0100

    Get Requests

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.status(201).send({ msg: "Hello, Wolrd" });
+});
+
+app.get("/api/users", (req, res) => {
+    res.send([
+        { id: 1, name: "Amine" },
+        { id: 2, name: "Zeki" },
+        { id: 3, name: "Abdnour" },
+    ]);
+});
+
+app.get("/api/products", (req, res) => {
+    res.send([
+        { id: 1, name: "Product1", price: 120.0 },
+        { id: 2, name: "Product2", price: 130.0 },
+        { id: 3, name: "Product3", price: 140.0 },
+    ]);
+})
